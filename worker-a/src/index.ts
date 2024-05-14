1cb3fdb31555c9a58e2e1b774e0298fc6e7c851f
Kevin Wang <kwangsan@gmail.com>
2024-05-14 14:17:33 -0400

    add try-catch to better reflect real setup; - also silence regular logs

--- worker-a/src/index.ts
+++ worker-a/src/index.ts
@@ -4,16 +4,31 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		ctx.waitUntil(
 			new Promise(async (resolve) => {
-				const text = await (env.WORKER_B as WorkerBService).getMessage();
-				console.log('[ctx.waitUntil + Promise] Worker B said:', text);
-				resolve(void 0);
+				try {
+					const text = await (env.WORKER_B as WorkerBService).getMessage();
+					// console.log('[ctx.waitUntil + Promise] Worker B said:', text);
+					resolve(void 0);
+				} catch (err) {
+					// This is the error that we're observing in Sentry
+					console.error(`[ctx.waitUntil + Promise] Worker B errored:`, err);
+
+					// ie. sentry.captureException(err);
+					resolve(void 0);
+				}
 			})
 		);
 
 		ctx.waitUntil(
 			(async () => {
-				const text = await (env.WORKER_B as WorkerBService).getMessage();
-				console.log('[ctx.waitUntil + async IIFE] Worker B said:', text);
+				try {
+					const text = await (env.WORKER_B as WorkerBService).getMessage();
+					// console.log('[ctx.waitUntil + async IIFE] Worker B said:', text);
+				} catch (err) {
+					// This is the error that we're observing in Sentry
+					console.error(`[ctx.waitUntil + async IIFE] Worker B errored:`, err);
+
+					// ie. sentry.captureException(err);
+				}
 			})()
 		);
 
